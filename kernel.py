03d29c7615e25ede17f995eb00309803f5cfed82
perpendicularai <fahd@perpendicular.web.za>
2024-07-24 09:53:35 -0700

    Kernel module updated and plugin module added.

--- kernel.py
+++ kernel.py
@@ -1,2 +1,21 @@
-class Kernel(list):
-     pass 
+# List class
+class MyList(list):
+     pass
+
+# Model class
+class Model(str):
+     pass
+
+# Template function that takes a string as input and returns a list
+def template(prompt:str, plugin):
+     my_list = MyList
+     my_list = [
+          {"role": "system", "content": plugin},
+          {"role": "user", "content": prompt},
+          ]
+     return my_list
+
+# Model function that returns the model as a string
+def model():
+     model = Model("<PATH TO GGUF MODEL>")
+     return model
